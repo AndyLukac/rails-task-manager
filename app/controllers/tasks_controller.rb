8bc82ae3f74ed208b947c624bb037898162d768f
Andrew Lukac a.lukac28@gmail.com <a.lukac28@gmail.com>
2018-08-14 11:02:11 +0200

    add task model

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,26 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = task.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+
+end
